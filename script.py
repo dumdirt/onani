0bd551c9d060e1048f73a7ad9bef1553560eb30a
Blakeando <52489043+Blakeando@users.noreply.github.com>
2020-08-03 00:32:55 +1000

    Increase death

--- script.py
+++ script.py
@@ -1,8 +1,12 @@
 import sys
 try:
-  if sys.argv[1] == "cum":
-    while True:
-      print("Fun is infinite :)" + chr(7))
+    if sys.argv[1] == "cum":
+        while True:
+            print("Fun is infinite :)" + chr(7))
 except:
-  while True:
-    print("Die" + chr(7))
+    try:
+        while True:
+            print("Die" + chr(7))
+    except:
+        while True:
+            print("Perish." + ''.join((chr(7) for x in range(666))))
