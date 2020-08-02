19f3e36c6e6f63e1d73b8da03afcd0f78d2a9be5
Blakeando <52489043+Blakeando@users.noreply.github.com>
2020-08-03 00:33:42 +1000

    Increase pands
    funny :)

--- pands.sh
+++ pands.sh
@@ -1,3 +1,12 @@
 while true; do
+    echo $RANDOM
+    tput bel
     echo 'I came my pands :)'
+    tput bel
+    echo $RANDOM
+    tput bel
+    echo $RANDOM
+    tput bel
+    echo $RANDOM
+    tput bel
 done
